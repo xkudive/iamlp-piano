a9dfeab5068c3d9736521df5418fdb8a91ebd4dc
xkudive <emin.yusupov.2006@mail.ru>
2023-03-13 00:49:41 +0500

    touchSlider

--- js/topSales.js
+++ js/topSales.js
@@ -1,4 +1,94 @@
 let topSalesSlider = document.querySelector(".topsales-goods-slider");
+
+salesSort.forEach((elem, index)=> {
+        let sliderItem = document.createElement("div");
+        let imageBox = document.createElement("a");
+        let imageBoxImage = document.createElement("img");
+        let imageBoxAdditions = document.createElement("div");
+        let promo = document.createElement("span");
+        let newAdded = document.createElement("span");
+        let promoImage = document.createElement("img");
+        let newImage = document.createElement("img");
+        let inStock = document.createElement("span");
+        let inStockImg = document.createElement("img");
+
+        let sliderItemInfo = document.createElement("div");
+        let sliderItemInfoName = document.createElement("span");
+        let sliderItemInfoAdditions = document.createElement("div");
+        let additionsLeft = document.createElement("div");
+        let additionsRight = document.createElement("div");
+        let additionsLeftImg = document.createElement("img");
+        let additionsLeftCost = document.createElement("span");
+        let additionsRightSettings = document.createElement("span");
+        let additionsRightFavourite = document.createElement("span");
+        let settingsImg = document.createElement("img");
+        let favouriteImg = document.createElement("img");
+
+        inStock.classList.add("in-stock");
+        imageBox.classList.add("card-img-box");
+        imageBoxAdditions.classList.add("card-img-box-additions");
+        newAdded.classList.add("new");
+        promo.classList.add("promo");
+        sliderItem.classList.add("topsales-slider-item");
+        sliderItemInfo.classList.add("topsales-slider-item-info");
+        sliderItemInfoName.classList.add("topsales-item-info-name");
+        sliderItemInfoAdditions.classList.add("topsales-item-info-additions");
+        additionsLeft.classList.add("item-info-additions-left");
+        additionsRight.classList.add("item-info-additions-right");
+        additionsLeftCost.classList.add("item-info-cost");
+
+        newImage.setAttribute("src",`images/trophy.svg`);
+        promoImage.setAttribute("src",`images/promotion.svg`);
+        imageBox.setAttribute("href", `${elem.url}`);
+        imageBoxImage.setAttribute("src",`images/${elem.searchImg}`); 
+        additionsLeftImg.setAttribute("src",`images/bag.svg`);
+        settingsImg.setAttribute("src",`images/settings.svg`);
+        favouriteImg.setAttribute("src",`images/favourite.svg`);
+
+        promo.innerText = "Акция";
+        newAdded.innerText = "Новинка";
+        if(elem.amount > 0){
+            inStockImg.setAttribute("src",`images/tick.svg`);
+            inStock.innerText = "В наличии";
+            inStock.prepend(inStockImg);
+        } else{
+            inStockImg.setAttribute("src",`images/x-button.svg`);
+            inStock.innerText = "Нет в наличии";
+            inStock.prepend(inStockImg);
+        }
+        sliderItemInfoName.innerText = `${elem.title + " " + elem.name + " " + elem.color}`
+        additionsLeftCost.innerText = `${elem.cost} ₽`;
+        additionsRightSettings.innerText = `0`;
+
+
+        promo.prepend(promoImage);
+        newAdded.prepend(newImage);
+        if(index === 0){
+            imageBoxAdditions.append(promo);
+        }
+        if(elem.id == salesSort.length){
+            imageBoxAdditions.append(newAdded);
+        }
+        imageBox.appendChild(imageBoxImage);
+        imageBox.appendChild(imageBoxAdditions);
+        imageBox.appendChild(inStock);
+        additionsRightSettings.prepend(settingsImg);
+        additionsRightFavourite.prepend(favouriteImg);
+        additionsRight.appendChild(additionsRightSettings);
+        additionsRight.appendChild(additionsRightFavourite);
+        additionsLeft.appendChild(additionsLeftImg);
+        additionsLeft.appendChild(additionsLeftCost);
+        sliderItemInfoAdditions.appendChild(additionsLeft);
+        sliderItemInfoAdditions.appendChild(additionsRight);
+        sliderItemInfo.appendChild(sliderItemInfoName);
+        sliderItemInfo.appendChild(sliderItemInfoAdditions);
+
+        sliderItem.appendChild(imageBox);
+        sliderItem.appendChild(sliderItemInfo);
+
+        topSalesSlider.appendChild(sliderItem);
+});
+
 let sliderBox = document.querySelector(".slider-box");
 let pressed = false;
 let startX = 0;
@@ -7,6 +97,7 @@ let boxPos = 0;
 let sliderPos = 0;
 
 topSalesSlider.addEventListener("touchstart", (e) => {
+    document.body.style.overflow = "hidden";
     pressed = true;
     boxPos = sliderBox.getBoundingClientRect().x;
     startX = e.touches[0].clientX;
@@ -18,6 +109,7 @@ sliderBox.addEventListener("touchcancel", (e) => {
 });
 
 topSalesSlider.addEventListener("touchend", (e) => {
+    document.body.style.overflow = "visible";
     pressed = false;
     if (window.innerWidth < (window.innerWidth - (topSalesSlider.getBoundingClientRect().width + topSalesSlider.getBoundingClientRect().x - (sliderBox.getBoundingClientRect().width + boxPos)))){
         topSalesSlider.style.transform = `translateX(${-(topSalesSlider.getBoundingClientRect().width - sliderBox.getBoundingClientRect().width)}px)`;
@@ -32,3 +124,30 @@ topSalesSlider.addEventListener("touchmove", (e) => {
     sliderPos = topSalesSlider.getBoundingClientRect().x;
     topSalesSlider.style.transform = `translateX(${(startX - e.touches[0].clientX) * -1 + px}px)`;
 });
+
+let topSalesSliderIndicator = document.querySelector(".sales-slider-indicator");
+let sliderLeftBtn = document.querySelector(".slider-left-button");
+let sliderRightBtn = document.querySelector(".slider-right-button");
+sliderLeftBtn.addEventListener("click", () => {
+    let sliderTransform = Number((topSalesSlider.style.transform).replace("translateX(", "").replace("px)", ""));
+    topSalesSlider.style.transform = `translateX(${sliderTransform + 300}px)`;
+    setTimeout(() => {
+        boxPos = sliderBox.getBoundingClientRect().x;
+        sliderPos = topSalesSlider.getBoundingClientRect().x;
+        if (sliderPos > boxPos) {
+            topSalesSlider.style.transform = `translateX(0px)`;
+        }
+    }, 200)
+})
+sliderRightBtn.addEventListener("click", () => {
+    let sliderTransform = Number((topSalesSlider.style.transform).replace("translateX(", "").replace("px)", ""));
+    topSalesSlider.style.transform = `translateX(${sliderTransform - 300}px)`;
+    setTimeout(() => {
+        boxPos = sliderBox.getBoundingClientRect().x;
+        sliderPos = topSalesSlider.getBoundingClientRect().x;
+        if (window.innerWidth < (window.innerWidth - (topSalesSlider.getBoundingClientRect().width + topSalesSlider.getBoundingClientRect().x - (sliderBox.getBoundingClientRect().width + boxPos)))){
+            topSalesSlider.style.transform = `translateX(${-(topSalesSlider.getBoundingClientRect().width - sliderBox.getBoundingClientRect().width)}px)`;
+        };
+    }, 200)
+})
+for(let i = 0; i < Array.from(topSalesSlider.children).length; i++) topSalesSliderIndicator.appendChild(document.createElement("span"));
